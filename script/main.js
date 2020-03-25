a0db9c16cf058888c95a5347c30800351a18a591
Paolo Francesco Marino <paolo.francesco.marino@gmail.com>
2020-03-25 14:34:58 +0100

    JS complete (to-do style-css)

--- script/main.js
+++ script/main.js
@@ -1,1 +1,32 @@
-console.log('js working')
+/**
+ * Password Generator
+ */
+
+// 1-chiedere nome utente
+
+var name = prompt('Qual è il tuo nome?');
+
+console.log(name);
+
+// 2-chiedere cognome utente
+
+var surname = prompt('Qaul è il tuo cognome?');
+
+console.log(surname);
+
+// 3-chiedere colore preferito
+
+var color = prompt('Colore Preferito?');
+
+console.log(color);
+
+
+// 3.1 - aggiunta numero finale
+
+var number = 20;
+
+console.log(number)
+
+// 4-scrivere nella pagina le informazioni ottenute 
+
+document.getElementById('result') .innerHTML = name + surname + color + number;
